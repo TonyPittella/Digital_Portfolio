09a07c70a0d623a4a2e5b71ef3e68e883ec1ec5b
TonyPittella <81885940+TonyPittella@users.noreply.github.com>
2021-04-28 21:26:00 -0400

    EON 3/4
    Add a footer and a picture

--- TonySite.js
+++ TonySite.js
@@ -1,1 +1,13 @@
-document.getElementsByClassName("------").addEventListener("click") 
+TheButton = document.getElementById("topBtn");
+window.onscroll = function() {scrollFun()};
+function scrollFun(){
+        if (document.body.scrollTop > 20 || document.documentElement.scrollTop >20) {
+            TheButton.style.display = "block";
+        }else{
+            TheButton.style.display = "none";
+        }
+}
+function topFun(){
+    document.body.scrollTop =0;
+    document.documentElement.scrollTop = 0;
+}
